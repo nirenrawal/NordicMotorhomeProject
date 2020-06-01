a175b9727f20c681f917b3f5c8098331f6237298
DavidKrtolica <david.krtolica8@gmail.com>
2020-06-01 10:06:09 +0200

    Added cancellation function (home controller method and html files) and made some changes in html and css.

--- src/main/java/com/example/demo/Controller/HomeController.java
+++ src/main/java/com/example/demo/Controller/HomeController.java
@@ -30,6 +30,7 @@ public class HomeController {
     int daysUp;
     double totalPriceUp;
     String last_nameUp;
+
     @Autowired
     CustomerService customerService;
     @Autowired
@@ -212,8 +213,9 @@ public class HomeController {
         return "redirect:/rentalContract";
     }
 
-    // ----------------------------------------------------------------------------------
-    // ----------------------------------------------------------------------------------
+
+    // ------------------------------------------------------------------------------------------------------------
+    // ------------------------------------------------------------------------------------------------------------
 
     @GetMapping("/payment")
     public String makePayment() {
@@ -502,4 +504,237 @@ public class HomeController {
         model.addAttribute("finalPrice", totalPriceUp);
         return "home/payment/end";
     }
+
+
+// ------------------------------------------------------------------------------------------------------------
+// ------------------------------------------------------------------------------------------------------------
+    //CANCELLATION
+    @GetMapping("/cancellation")
+    public String cancellation(){
+        return "home/cancellation";
+    }
+
+    @PostMapping("/cancellationBill")
+    public String cancellationBill(HttpServletRequest request, Model m){
+        String userInputID = request.getParameter("inputID");
+        String userInputDate = request.getParameter("inputDate");
+        int id = Integer.parseInt(userInputID);
+        String date = "";
+        int motID = 0;
+
+        List<RentalContract> rentalContractList = rentalContractService.viewAllRentalContract();
+        for(int i = 0; i < rentalContractList.size(); i++){
+            if(rentalContractList.get(i).getRentalContract_id()==id){
+                date = rentalContractList.get(i).getRentalContract_startDate();
+                motID = rentalContractList.get(i).getMotorhome_id();
+            }
+        }
+        String date1 = userInputDate;          // today
+        String date2 = date;                  // rentalContract_startDate
+
+        // Date 1 - Getting days/months/years
+        int d1day10 = parseInt(String.valueOf(date1.charAt(0)));
+        int d1day1 = parseInt(String.valueOf(date1.charAt(1)));
+        int d1days = d1day10 * 10 + d1day1;
+
+        int d1month10 = parseInt(String.valueOf(date1.charAt(3)));
+        int d1month1 = parseInt(String.valueOf(date1.charAt(4)));
+        int d1months = d1month10 * 10 + d1month1;
+
+        int d1year1000 = parseInt(String.valueOf(date1.charAt(6)));
+        int d1year100 = parseInt(String.valueOf(date1.charAt(7)));
+        int d1year10 = parseInt(String.valueOf(date1.charAt(8)));
+        int d1year1 = parseInt(String.valueOf(date1.charAt(9)));
+        int d1years = d1year1000 * 1000 + d1year100 * 100 + d1year10 * 10 + d1year1;
+
+        // Date 2 - Getting days/months/years
+        int d2day10 = parseInt(String.valueOf(date2.charAt(0)));
+        int d2day1 = parseInt(String.valueOf(date2.charAt(1)));
+        int d2days = d2day10 * 10 + d2day1;
+
+        int d2month10 = parseInt(String.valueOf(date2.charAt(3)));
+        int d2month1 = parseInt(String.valueOf(date2.charAt(4)));
+        int d2months = d2month10 * 10 + d2month1;
+
+        int d2year1000 = parseInt(String.valueOf(date2.charAt(6)));
+        int d2year100 = parseInt(String.valueOf(date2.charAt(7)));
+        int d2year10 = parseInt(String.valueOf(date2.charAt(8)));
+        int d2year1 = parseInt(String.valueOf(date2.charAt(9)));
+        int d2years = d2year1000 * 1000 + d2year100 * 100 + d2year10 * 10 + d2year1;
+
+        String d1date = d1days + "/" + d1months + "/" + d1years;
+        String d2date = d2days + "/" + d2months + "/" + d2years;
+
+
+        // FIND NUMBER OF DAYS
+        int cancelNumberOfDays = 0;
+        // months == and years == (10/12/2020 - 20/12/2020)
+        if ((d1months == d2months) && (d1years == d2years)) {
+            cancelNumberOfDays = d2days - d1days;
+        } else if ((d1days < d2days) && (d1months < d2months) && (d1years == d2years)) {                    // (11/01/2020 - 22/10/2020)
+            cancelNumberOfDays = (d2days - d1days) + 30 * (d2months - d1months);
+        } else if ((d1days > d2days) && (d1months < d2months) && (d1years == d2years)) {                    // (22/01/2020 - 11/10/2020)
+            cancelNumberOfDays = ((30 - d1days) + d2days) + 30 * (d2months - d1months - 1);
+        } else if ((d1days == d2days) && (d1months < d2months) && (d1years == d2years)) {                     // (11/01/2020 - 11/10/2020)
+            cancelNumberOfDays = 30 * (d2months - d1months);
+        } else if ((d1days < d2days) && (d1months > d2months) && (d1years < d2years)) {                     // (11/10/2019 - 22/01/2020)
+            cancelNumberOfDays = ((12 - d1months) * 30 + 30 - d1days) + ((d2months - 1) * 30 + d2days) + ((d2years - d1years) - 1) * 360;
+        } else if ((d1days > d2days) && (d1months < d2months) && (d1years < d2years)) {                           // (22/01/2019 - 11/10/2020)
+            cancelNumberOfDays = ((30 - d1days) + d2days) + (d2months - (d1months + 1)) * 30 + (d2years - d1years) * 360;
+        } else if ((d1days < d2days) && (d1months < d2months) && (d1years < d2years)) {                     // (11/01/2019 - 22/10/2020)
+            cancelNumberOfDays = (30 - d1days + d2days) + (d2months - d1months) * 30 + (d2years - d1years) * 360;
+        } else if ((d1days > d2days) && (d1months > d2months) && (d1years < d2years)) {                           // (22/10/2019 - 11/01/2020)
+            cancelNumberOfDays = ((30 - d1days) + d2days) + 30 * ((12 - d1months) - 1) + 30 * d2months;
+        }
+
+        // add cancel number of days to the model
+        m.addAttribute("cancelNumberOfDays", cancelNumberOfDays);
+
+
+        String startDate = "";
+        String endDate = "";
+        for (RentalContract rentalContract : rentalContractList) {
+            if (rentalContract.getRentalContract_id() == id) {
+                startDate = rentalContract.getRentalContract_startDate();
+                endDate = rentalContract.getRentalContract_endDate();
+            }
+        }
+
+        String date3 = startDate;                // rentalContract_startDate
+        String date4 = endDate;                  // rentalContract_endDate
+
+        // Date 3 - Getting days/months/years
+        int d3day10 = parseInt(String.valueOf(date3.charAt(0)));
+        int d3day1 = parseInt(String.valueOf(date3.charAt(1)));
+        int d3days = d3day10 * 10 + d3day1;
+
+        int d3month10 = parseInt(String.valueOf(date3.charAt(3)));
+        int d3month1 = parseInt(String.valueOf(date3.charAt(4)));
+        int d3months = d3month10 * 10 + d3month1;
+
+        int d3year1000 = parseInt(String.valueOf(date3.charAt(6)));
+        int d3year100 = parseInt(String.valueOf(date3.charAt(7)));
+        int d3year10 = parseInt(String.valueOf(date3.charAt(8)));
+        int d3year1 = parseInt(String.valueOf(date3.charAt(9)));
+        int d3years = d3year1000 * 1000 + d3year100 * 100 + d3year10 * 10 + d3year1;
+
+        // Date 4 - Getting days/months/years
+        int d4day10 = parseInt(String.valueOf(date4.charAt(0)));
+        int d4day1 = parseInt(String.valueOf(date4.charAt(1)));
+        int d4days = d4day10 * 10 + d4day1;
+
+        int d4month10 = parseInt(String.valueOf(date4.charAt(3)));
+        int d4month1 = parseInt(String.valueOf(date4.charAt(4)));
+        int d4months = d4month10 * 10 + d4month1;
+
+        int d4year1000 = parseInt(String.valueOf(date4.charAt(6)));
+        int d4year100 = parseInt(String.valueOf(date4.charAt(7)));
+        int d4year10 = parseInt(String.valueOf(date4.charAt(8)));
+        int d4year1 = parseInt(String.valueOf(date4.charAt(9)));
+        int d4years = d4year1000 * 1000 + d4year100 * 100 + d4year10 * 10 + d4year1;
+
+        String d3date = d3days + "/" + d3months + "/" + d3years;
+        String d4date = d4days + "/" + d4months + "/" + d4years;
+
+
+        // FIND NUMBER OF DAYS
+        int numberOfDays = 0;
+        // months == and years == (10/12/2020 - 20/12/2020)
+        if ((d3months == d4months) && (d3years == d4years)) {
+            numberOfDays = d4days - d3days;
+        } else if ((d3days < d4days) && (d3months < d4months) && (d3years == d4years)) {                    // (11/01/2020 - 22/10/2020)
+            numberOfDays = (d4days - d3days) + 30 * (d4months - d3months);
+        } else if ((d3days > d4days) && (d3months < d4months) && (d3years == d4years)) {                    // (22/01/2020 - 11/10/2020)
+            numberOfDays = ((30 - d3days) + d4days) + 30 * (d4months - d3months - 1);
+        } else if ((d3days == d4days) && (d3months < d4months) && (d3years == d4years)) {                     // (11/01/2020 - 11/10/2020)
+            numberOfDays = 30 * (d4months - d3months);
+        } else if ((d3days < d4days) && (d3months > d4months) && (d3years < d4years)) {                     // (11/10/2019 - 22/01/2020)
+            numberOfDays = ((12 - d3months) * 30 + 30 - d3days) + ((d4months - 1) * 30 + d4days) + ((d4years - d3years) - 1) * 360;
+        } else if ((d3days > d4days) && (d3months < d4months) && (d3years < d4years)) {                           // (22/01/2019 - 11/10/2020)
+            numberOfDays = ((30 - d3days) + d4days) + (d4months - (d3months + 1)) * 30 + (d4years - d3years) * 360;
+        } else if ((d3days < d4days) && (d3months < d4months) && (d3years < d4years)) {                     // (11/01/2019 - 22/10/2020)
+            numberOfDays = (30 - d3days + d4days) + (d4months - d3months) * 30 + (d4years - d3years) * 360;
+        } else if ((d3days > d4days) && (d3months > d4months) && (d3years < d4years)) {                           // (22/10/2019 - 11/01/2020)
+            numberOfDays = ((30 - d3days) + d4days) + 30 * ((12 - d3months) - 1) + 30 * d4months;
+        }
+
+        List<Motorhome> motorhomeList = motorhomeService.viewAllMotorhome();
+        String brand ="";
+        String model = "";
+        for(int i = 0; i < motorhomeList.size(); i++){
+            if(motorhomeList.get(i).getMotorhome_id()==motID){
+                brand = motorhomeList.get(i).getMotorhome_brand();
+                model = motorhomeList.get(i).getMotorhome_model();
+            }
+        }
+
+        // INITIATE ALL THE PRICES
+        // sum of all prices
+        double totalPrice = 0;
+
+        // seasonal prices
+        int pricePerDaySeason = 0;
+        int seasonPrice = 0;
+
+        // motorhome prices
+        int brandPrice = 0;
+        int pricePerDayBrand = 0;
+        int modelPrice = 0;
+        int pricePerDayModel = 0;
+
+
+        // 1. Price for all days (Season)
+        if ((d3months == 1) || (d3months == 2) || (d3months == 111) || (d3months == 12)) {        // Low season
+            pricePerDaySeason = 100;
+        } else if ((d3months == 3) || (d3months == 4) || (d3months == 9) || (d3months == 10)) {  // Peak season
+            pricePerDaySeason = 130;
+        } else if ((d3months == 5) || (d3months == 6) || (d3months == 7) || (d3months == 8)) {   // Mid season
+            pricePerDaySeason = 160;
+        }
+        seasonPrice = numberOfDays * pricePerDaySeason;
+        totalPrice += seasonPrice;
+
+
+        // 2. Price for all days (Brand)
+        String motorhomeBrand = brand;
+        if (motorhomeBrand.equalsIgnoreCase("Winnebago")) {
+            pricePerDayBrand = 100;
+        } else if (motorhomeBrand.equalsIgnoreCase("Entegra")) {
+            pricePerDayBrand = 150;
+        }
+        brandPrice = numberOfDays * pricePerDayBrand;
+        totalPrice += brandPrice;
+
+
+        // 3. Price for all days (Model)
+        String motorhomeModel = model;
+        if (motorhomeModel.equalsIgnoreCase("Boldt")) {
+            pricePerDayModel = 10;
+        } else if (motorhomeModel.equalsIgnoreCase("Travato")) {
+            pricePerDayModel = 100;
+        } else if (motorhomeModel.equalsIgnoreCase("Anthem")) {
+            pricePerDayModel = 50;
+        } else if (motorhomeModel.equalsIgnoreCase("Emblem ")) {
+            pricePerDayModel = 150;
+        }
+        modelPrice = numberOfDays * pricePerDayModel;
+        totalPrice += modelPrice;
+
+        double cancelFee = 0.0;
+        if(cancelNumberOfDays >= 50){
+            cancelFee = 0.2 * totalPrice;
+        } else if(cancelNumberOfDays >= 15 && cancelNumberOfDays <= 49){
+            cancelFee = 0.5 * totalPrice;
+        } else if(cancelNumberOfDays < 15 && cancelNumberOfDays >= 1){
+            cancelFee = 0.8 * totalPrice;
+        } else if(cancelNumberOfDays < 1){
+            cancelFee = 0.95 * totalPrice;
+        }
+
+        double finalFee = cancelFee;
+        m.addAttribute("finalCancelFee", finalFee);
+
+        return "home/cancellationBill";
+    }
+
 }
